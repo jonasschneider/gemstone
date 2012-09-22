ae268130930791cfac73ad057dc856e1c7e9194e
Jonas Schneider <mail@jonasschneider.com>
2012-09-22 12:43:44 +0200

    fix transformation spec

--- spec/unwind_stack_spec.rb
+++ spec/unwind_stack_spec.rb
@@ -3,22 +3,16 @@ require 'gemstone'
 describe Gemstone::Transformations::UnwindStack do
   it "can unwind nested kernel calls" do
     r = described_class.apply [:kernel, [[:pi_lit_str, "puts"], [:send, :kernel, [[:pi_lit_str, "returnstr"], [:pi_lit_str, "Stackstring"]]]]]
-    r.should == [
-      [:ps_push],
-      [:ps_push],
-
+    r.should eq([
       [:ps_pusharg, [:pi_lit_str, "Stackstring"]],
       [:ps_pusharg, [:pi_lit_str, "returnstr"]],
       [:ps_kernel_dispatch],
 
-      [:ps_pop],
+      [:ps_pusharg, [:pi_get_result]],
 
-      [:ps_pusharg, [:pi_get_inner_res]],
       [:ps_pusharg, [:pi_lit_str, "puts"]],
-      [:ps_kernel_dispatch],
-
-      [:ps_pop]
-    ]
+      [:ps_kernel_dispatch]
+    ])
   end
 
   it "can unwind a :send to an object" do
@@ -26,23 +20,17 @@ describe Gemstone::Transformations::UnwindStack do
             [:send, :kernel, [[:pi_lit_str, "lvar_get"], [:pi_lit_str, "mystr"]]],
             [[:pi_lit_str, "my message"]]
           ]
-    r.should == [
-      [:ps_push],
-
+    r.should eq([
       [:ps_pusharg, [:pi_lit_str, "my message"]],
-
-      [:ps_push],
       [:ps_pusharg, [:pi_lit_str, "mystr"]],
       [:ps_pusharg, [:pi_lit_str, "lvar_get"]],
       [:ps_kernel_dispatch],
-      [:ps_pop],
-      [:ps_pusharg, [:pi_get_inner_res]],
 
-      [:ps_init_lscope],
-      
+      [:ps_pusharg, [:pi_get_result]],
+
+      [:ps_push_with_argstack_as_params],
       [:ps_object_dispatch],
-      
       [:ps_pop]
-    ]
+    ])
   end
 end
